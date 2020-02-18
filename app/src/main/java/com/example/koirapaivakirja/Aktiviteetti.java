aa9bcc1bf1d6438a5ded924ea85f883f6fd98471
AskoTJM <asko.mattila@gmail.com>
2020-02-18 11:12:26 +0200

    Versio 0.01

---
+++ app/src/main/java/com/example/koirapaivakirja/Aktiviteetti.java
@@ -0,0 +1,14 @@
+package com.example.koirapaivakirja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Aktiviteetti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aktiviteetti);
+    }
+}
