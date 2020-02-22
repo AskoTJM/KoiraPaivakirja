34f77e0b0894d6b2fa93cbcab0ecba414ad24a8b
AskoTJM <asko.mattila@gmail.com>
2020-02-22 19:01:19 +0200

    Versio 0.2

--- app/src/main/java/com/example/koirapaivakirja/Etusivu.java
+++ app/src/main/java/com/example/koirapaivakirja/Etusivu.java
@@ -2,7 +2,9 @@ package com.example.koirapaivakirja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Etusivu extends AppCompatActivity {
 
@@ -11,4 +13,24 @@ public class Etusivu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_etusivu);
     }
+
+    public void goToFeed(View view)
+    {
+        Intent intentFeed = new Intent(this, Ruokinta.class);
+        startActivity(intentFeed);
+    }
+
+    public void goToActivity(View view){
+        Intent intentActivity = new Intent(this, Aktiviteetti.class);
+        startActivity(intentActivity);
+    }
+
+    public void goToInfo(View view){
+
+    }
+
+    public void goToReport(View view){
+
+    }
+
 }
