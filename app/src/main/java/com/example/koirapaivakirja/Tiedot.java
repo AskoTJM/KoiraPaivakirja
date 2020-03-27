3a9645c10bb7e461d3061b783153461b0f3b1cab
AskoTJM <asko.mattila@gmail.com>
2020-03-27 17:18:11 +0200

    All the small things.

--- app/src/main/java/com/example/koirapaivakirja/Tiedot.java
+++ app/src/main/java/com/example/koirapaivakirja/Tiedot.java
@@ -1,14 +1,97 @@
 package com.example.koirapaivakirja;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Handler;
 import android.os.Bundle;
 
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
 public class Tiedot extends AppCompatActivity {
+
+    EditText mInfoName, mInfoNote, mInfoIDNumber, mInfoReg, mInfoBirth, mInfoKennelName, mInfoWeight;
+    ImageView mInfoImageView;
+    String infoMode = "Info"; // Modes = Info, Edit, Add
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiedot);
+        mInfoIDNumber = findViewById(R.id.infoIDNum);
+        mInfoBirth = findViewById(R.id.infoBirth);
+        mInfoKennelName = findViewById(R.id.infoKennelName);
+        mInfoName = findViewById(R.id.infoName);
+        mInfoReg = findViewById(R.id.infoReg);
+        mInfoNote = findViewById(R.id.infoNote);
+        mInfoWeight = findViewById(R.id.infoWeight);
+
+        mInfoImageView = findViewById(R.id.infoDogImage);
+
+        handler = new Handler();
+
+      // mInfoImageView.setOnClickListener();
+
+        toggleEditMode();
+        getDataFromFireStore();
+
     }
+
+    private void getDataFromFireStore(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("dogs").document("rKJvTSFsozBr0V5JAyvQ");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+   /*             if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+      //                  Log.d(this, "DocumentSnapshot data: " + document.getData().values());
+                    } else {
+                     //   Log.d(this, "No such document");
+                    }
+                } else {
+                   // Log.d(this, "get failed with ", task.getException());
+                }
+      */      }
+        });
+    }
+
+
+    private void toggleEditMode(){
+        switch(infoMode){
+            case "Info":
+                mInfoIDNumber.setEnabled(false);
+                mInfoBirth.setEnabled(false);
+                mInfoKennelName.setEnabled(false);
+                mInfoName.setEnabled(false);
+                mInfoReg.setEnabled(false);
+                mInfoNote.setEnabled(false);
+                mInfoWeight.setEnabled(false);
+                break;
+            case "Edit":
+                mInfoIDNumber.setEnabled(true);
+                mInfoBirth.setEnabled(true);
+                mInfoKennelName.setEnabled(true);
+                mInfoName.setEnabled(true);
+                mInfoReg.setEnabled(true);
+                mInfoNote.setEnabled(true);
+                mInfoWeight.setEnabled(true);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + infoMode);
+        }
+    }
+
+
+
 }
